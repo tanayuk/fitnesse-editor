a95f4ed19155da4cfa3a308e7d32cf00b0254e34
t.fukuzawa <t.fukuzawa@acbca073-9ec7-8df5-112a-cce40ae5b89d>
2011-05-26 15:59:04 +0000

    preference page draft

---
+++ trunk/fitedit/src/fitedit/preferences/PreferenceConstants.java
@@ -0,0 +1,12 @@
+package fitedit.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_FITNESSEROOT_DIR = "fitnesse_root_dir";
+
+	public static final String P_URL_PREFIX = "url_prefix";
+	
+}
